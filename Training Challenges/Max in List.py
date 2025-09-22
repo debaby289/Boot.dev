efb4b1c78ac45bbcd0c6e6ba9808cc03267f0b1e
Devine <simsdevine@gmail.com>
2025-09-21 23:16:04 -0500

    25.09.21

--- Training Challenges/Max in List.py
+++ Training Challenges/Max in List.py
@@ -1,3 +1,13 @@
+"""
+Max in List
+A list is an ordered collection of items, like [3, 7, 2].
+A loop lets you visit each item in the list one by one.
+Hints:
+
+Think about a variable that keeps track of your current best answer.
+Consider how you compare each new item to that current best.
+"""
+
 # python
 nums = [3, -2, 7, 4]  # assume non-empty
 current_max = nums[0]
