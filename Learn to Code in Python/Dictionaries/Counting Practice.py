83b9edd47fb32dd204ea653219305462b9683e73
Devine <simsdevine@gmail.com>
2025-09-28 18:20:56 -0500

    25.09.28

--- Learn to Code in Python/Dictionaries/Counting Practice.py
+++ Learn to Code in Python/Dictionaries/Counting Practice.py
@@ -1,3 +1,14 @@
+"""
+Assignment
+We need to be able to report to our players how many enemies are in their immediate vicinity - but they want the count of each enemy by its kind. Fix the count_enemies function. It accepts as input:
+    enemy_names: a list of strings.
+It should return a dictionary where:
+    The keys are all the enemy names from the list
+    The values are the counts of how many times each enemy appeared in the list.
+        Run the code in its current state. It will raise a KeyError.
+        Fix the code.
+"""
+
 def count_enemies(enemy_names):
     enemies_dict = {}
     for enemy_name in enemy_names:
